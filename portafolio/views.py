17f6caa1d487cdfe31745db188c015e99345df40
Mauricio Correa <mauriciocorrea.1015@gmail.com>
2024-01-11 21:12:20 -0500

    init of repositori of the project of briefcase

---
+++ portafolio/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import project# Create your views here.
+def home(request):
+    projects = project.objects.all()
+    return render(request, 'home.html', {'projects':projects}) 
